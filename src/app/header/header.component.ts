f9103ed353aab934dd3f41d40ad79130688fdec6
Alan Berns <alan_berns@yahoo.com.ar>
2025-09-12 12:56:21 -0300

    mejora del header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,10 +9,33 @@ import { RouterModule } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+
+export class HeaderComponent implements OnInit {
   menuOpen = false;
+  scrolled = false;
+  isDesktop = window.innerWidth >= 640; // Tailwind sm breakpoint
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+
+  @HostListener('window:scroll')
+  onScroll() {
+    this.scrolled = window.scrollY > 50;
+
+    // Si volvés al top y estás en desktop, cerramos el menú hamburguesa
+    if (!this.scrolled && this.isDesktop && this.menuOpen) {
+      this.menuOpen = false;
+    }
+  }
+
+
+  @HostListener('window:resize')
+  onResize() {
+    this.isDesktop = window.innerWidth >= 640;
+  }
+
+  ngOnInit() {
+    this.onResize();
   }
 }
