c27dc900288ae09a6d56f4c290588711374507ad
Alan Berns <alan_berns@yahoo.com.ar>
2025-09-11 23:13:54 -0300

    scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +15,11 @@ import { HeaderComponent } from './header/header.component';
 })
 export class AppComponent {
   title = 'portfolio';
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' }); // 👈 scroll suave al top
+      });
+  }
 }
